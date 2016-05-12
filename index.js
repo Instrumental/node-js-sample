e39b5f6eda9d832af20667ada3832cf8e55f5924
Jason Gladish <jason@expectedbehavior.com>
2016-05-12 16:39:09 -0400

    Add Instrumental to the project.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var express = require('express')
 var app = express()
+var I = require('instrumental-node');
+I.configure({
+  // from here: https://instrumentalapp.com/docs/tokens
+  apiKey:  process.env.INSTRUMENTAL_API_KEY, // 'project_api_token',
+
+  // optional, default shown
+  host:    'collector.instrumentalapp.com',
+
+  // optional, default shown
+  enabled: true,
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+  I.increment('example.node.hello.worlds' /*, value = 1, time = now, count = 1 */);
   response.send('Hello World!')
 })
 
